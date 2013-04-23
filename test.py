4be97ff0c495cdcc58eb58525b48fce84fa49cb5
Mariano Vassallo <mvassallo@ba.gov.ar>
2013-04-23 15:20:08 -0300

    procesar el estado de trenes para guardarlo en json

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+import datetime
+import json
 import urllib2
 import pprint
+import ipdb
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -16,6 +19,7 @@ def procesa_posiciones_trenes(response):
     if arr.__len__() > 0:
         cant = int(arr[0])
         m = 1
+        markers = []
         for n in range(1, cant):
             marker = {
                 'id': arr[m],
@@ -34,12 +38,18 @@ def procesa_posiciones_trenes(response):
                 marker['destino'] = 'Retiro'
             else:
                 marker['destino'] = 'Tigre'
+            markers.append( marker )
             m = m + 13
+        return { "time": json.dumps(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')), "markers" : markers }
 
-response = urllib2.urlopen(SERVICE_URI)
-html = response.read()
+html = ""
+# a veces este servicio devuelve 0 bytes, aunque con code 200 OK
+while not html: 
+  response = urllib2.urlopen(SERVICE_URI)
+  html = response.read()
 
-procesa_posiciones_trenes(html)
+
+print json.dumps(procesa_posiciones_trenes(html)) 
 
 # Respuesta ajax de widget
 
@@ -52,7 +62,12 @@ LINEAS = {
             'Ituzaingo', 'Padua', 'Merlo', 'Paso del Rey', 'Moreno'
         ]
     },
-    'mitre_tigre': 5,
+    'mitre_tigre': {
+      'id': 5,
+      'estaciones': [
+         'Retiro','L. de la Torre','Belgrano c','Nu&ntilde;ez','Rivadavia','Vicente L&oacute;pez','Olivos','La Lucila','Mart&iacute;nez','Acassuso','San Isidro c','B&eacute;ccar','Victoria','Virreyes','San Fernando c','Carup&aacute','Tigre'
+        ]
+    },
     'mitre_mitre': 7,
     'mitre_suarez': 9,
 }
@@ -73,22 +88,35 @@ html = response.read()
 def check_tiempos(response, estacion, estaciones):
     arr = response.split("_")
     if arr.__len__() > 0:
+      try:
         cant = int(arr[0])
         if cant > 0 and estacion > 0:
             index = estaciones.index(estacion)
             print 'Estacion %s' % estacion
             print 'Sentido %s' % estaciones[-1]
-            print 'Proximo: %s' % arr[index*6+1]
-            print 'Siguiente: %s' % arr[index*6+2]
-            print 'Siguiente: %s' % arr[index*6+3]
+            # no tengo claro en que condiciones el array viene mas corto que lo esperado
+            print 'Proximo: %s' %   ( arr[index*6+1] if (index*6+1 < len(arr) ) else "-" )
+            print 'Siguiente: %s' % ( arr[index*6+2] if (index*6+2 < len(arr) ) else "-")
+            print 'Siguiente: %s' % ( arr[index*6+3] if (index*6+3 < len(arr) ) else "-")
+
             print 'Sentido %s' % estaciones[0]
-            print 'Proximo: %s' % arr[index*6+4]
-            print 'Siguiente: %s' % arr[index*6+5]
-            print 'Siguiente: %s' % arr[index*6+6]
+
+            print 'Proximo: %s' %   ( arr[index*6+4] if (index*6+4 < len(arr) ) else "-" )
+            print 'Siguiente: %s' % ( arr[index*6+5] if (index*6+5 < len(arr) ) else "-")
+            print 'Siguiente: %s' % ( arr[index*6+6] if (index*6+6 < len(arr) ) else "-")
+
+
             print '\n'
+      except:
+            ipdb.set_trace()
 
 
 estaciones = LINEAS['sarmiento']['estaciones']
-
-for estacion in estaciones:
-    check_tiempos(html, estacion, estaciones)
+# 
+# for estacion in estaciones:
+#     check_tiempos(html, estacion, estaciones)
+# 
+# estaciones = LINEAS['mitre_tigre']['estaciones']
+# 
+# for estacion in estaciones:
+#     check_tiempos(html, estacion, estaciones)
